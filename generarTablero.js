93da462fe11dd1f9ecfdfd34736767ea638adcc2
augusto-rehfeldt <augusto.rehfeldt.socio@gmail.com>
2021-07-23 16:14:35 -0300

    primer release

---
+++ generarTablero.js
@@ -0,0 +1,198 @@
+Array.range = function(a, b, step){
+    var A = [];
+    if(typeof a == 'number'){
+        A[0] = a;
+        step = step || 1;
+        while(a+step <= b){
+            A[A.length]= a+= step;
+        }
+    }
+    else {
+        var s = 'abcdefghijklmnñopqrstuvwxyz';
+        if(a === a.toUpperCase()){
+            b = b.toUpperCase();
+            s = s.toUpperCase();
+        }
+        s = s.substring(s.indexOf(a), s.indexOf(b)+ 1);
+        A = s.split('');        
+    }
+    return A;
+}
+
+function crearTablero() {
+    var tablero = []
+
+    for (let i of Array.range(0, 8)) {
+        tablero.push([0, 0, 0, 0, 0, 0, 0, 0, 0])
+    }
+
+    return tablero
+}
+
+function enCuadrilatero(numero, i, k, tablero) {
+    var cuadrilateros = {
+        "0": [tablero[0][0], tablero[0][1], tablero[0][2], 
+        tablero[1][0], tablero[1][1], tablero[1][2],
+        tablero[2][0], tablero[2][1], tablero[2][2]],
+        "1": [tablero[0][3], tablero[0][4], tablero[0][5], 
+        tablero[1][3], tablero[1][4], tablero[1][5],
+        tablero[2][3], tablero[2][4], tablero[2][5]],
+        "2": [tablero[0][6], tablero[0][7], tablero[0][8], 
+        tablero[1][6], tablero[1][7], tablero[1][8],
+        tablero[2][6], tablero[2][7], tablero[2][8]],
+        "3": [tablero[3][0], tablero[3][1], tablero[3][2], 
+        tablero[4][0], tablero[4][1], tablero[4][2],
+        tablero[5][0], tablero[5][1], tablero[5][2]],
+        "4": [tablero[3][3], tablero[3][4], tablero[3][5], 
+        tablero[4][3], tablero[4][4], tablero[4][5],
+        tablero[5][3], tablero[5][4], tablero[5][5]],
+        "5": [tablero[3][6], tablero[3][7], tablero[3][8], 
+        tablero[4][6], tablero[4][7], tablero[4][8],
+        tablero[5][6], tablero[5][7], tablero[5][8]],
+        "6": [tablero[6][0], tablero[6][1], tablero[6][2], 
+        tablero[7][0], tablero[7][1], tablero[7][2],
+        tablero[8][0], tablero[8][1], tablero[8][2]],
+        "7": [tablero[6][3], tablero[6][4], tablero[6][5], 
+        tablero[7][3], tablero[7][4], tablero[7][5],
+        tablero[8][3], tablero[8][4], tablero[8][5]],
+        "8": [tablero[6][6], tablero[6][7], tablero[6][8], 
+        tablero[7][6], tablero[7][7], tablero[7][8],
+        tablero[8][6], tablero[8][7], tablero[8][8]],
+    }
+
+    if (0 <= i && i <= 2) {
+        if (0 <= k && k <= 2) {
+            if (cuadrilateros["0"].includes(numero)) {
+                return true
+            }
+        }
+        else if (3 <= k && k <= 5) {
+            if (cuadrilateros["1"].includes(numero)) {
+                return true
+            }
+        }
+        else {
+            if (cuadrilateros["2"].includes(numero)) {
+                return true
+            }
+        }
+    }
+    else if (3 <= i && i <= 5) {
+        if (0 <= k && k <= 2) {
+            if (cuadrilateros["3"].includes(numero)) {
+                return true
+            }
+        }
+        else if (3 <= k && k <= 5) {
+            if (cuadrilateros["4"].includes(numero)) {
+                return true
+            }
+        }
+        else {
+            if (cuadrilateros["5"].includes(numero)) {
+                return true
+            }
+        }
+    }
+    else {
+        if (0 <= k && k <= 2) {
+            if (cuadrilateros["6"].includes(numero)) {
+                return true
+            }
+        }
+        else if (3 <= k && k <= 5) {
+            if (cuadrilateros["7"].includes(numero)) {
+                return true
+            }
+        }
+        else {
+            if (cuadrilateros["8"].includes(numero)) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
+
+function rellenarTablero(tablero) {
+    for (let i of Array.range(0, 8)) {
+        for (let k of Array.range(0, 8)) {
+            var numeros_a_utilizar = Array.range(1, 9)
+            while (true) {
+                if (numeros_a_utilizar.length === 0) {
+                    return false
+                }
+                var numero = numeros_a_utilizar[
+                    Math.floor(Math.random()*numeros_a_utilizar.length)
+                ]
+                if (tablero[i].includes(numero)) {
+                    var index = numeros_a_utilizar.indexOf(numero)
+                    numeros_a_utilizar.splice(index, 1)
+                    continue
+                } else {
+                    var flag = false
+                    for (let j of Array.range(0, 8)) {
+                        if (numero === tablero[j][k]) {
+                            var index = numeros_a_utilizar.indexOf(numero)
+                            numeros_a_utilizar.splice(index, 1)
+                            flag = true
+                            break
+                        }
+                    }
+                    if (flag) {
+                        continue
+                    }
+                    if (enCuadrilatero(numero, i, k, tablero)) {
+                        var index = numeros_a_utilizar.indexOf(numero)
+                        numeros_a_utilizar.splice(index, 1)
+                        continue
+                    }
+                    var index = numeros_a_utilizar.indexOf(numero)
+                    numeros_a_utilizar.splice(index, 1)
+                    tablero[i][k] = numero
+                    break
+                }
+            }
+        }
+    }
+
+    return tablero
+}
+
+
+export function nuevoTablero() {
+    while (true) {
+        var tablero = rellenarTablero(crearTablero())
+        if (tablero) {
+            return tablero
+        }
+    }
+}
+
+function sumarValores(arr) {
+    var suma = 0
+    for (let valor of arr) {
+        suma += valor
+    }
+    return suma
+}
+
+/* var tiempos = []
+
+start = +new Date();
+
+for (let i of Array.range(1, 1000)) {
+    start2 = +new Date();
+    nuevoTablero()
+    console.log(i)
+    end2 = +new Date();
+    tiempos.push(end2 - start2)
+}
+
+end = +new Date();
+
+console.log("generado en:", (end - start)/1000, "s")
+console.log("tiempo promedio por iteración:", (sumarValores(tiempos)/tiempos.length)/1000, "s")
+console.log("Tiempo mínimo de rellenado:", Math.min(...tiempos)/1000, "s")
+console.log("Tiempo máximo de rellenado:", Math.max(...tiempos)/1000, "s") */
---
+++ index.js
@@ -0,0 +1,403 @@
+import { nuevoTablero } from "./generarTablero.js"
+
+Array.range = function(a, b, step){
+    var A = [];
+    if(typeof a == 'number'){
+        A[0] = a;
+        step = step || 1;
+        while(a+step <= b){
+            A[A.length]= a+= step;
+        }
+    }
+    else {
+        var s = 'abcdefghijklmnñopqrstuvwxyz';
+        if(a === a.toUpperCase()){
+            b = b.toUpperCase();
+            s = s.toUpperCase();
+        }
+        s = s.substring(s.indexOf(a), s.indexOf(b)+ 1);
+        A = s.split('');        
+    }
+    return A;
+}
+
+const arrayWithoutElementAtIndex = function (arr, index) {
+    return arr.filter(function(value, arrIndex) {
+      return index !== arrIndex;
+    });
+  }
+
+function enCuadrilatero(numero, i, k, inputs) {
+    let cuadrilateros = {
+        0: [inputs[0][0].value, inputs[0][1].value, inputs[0][2].value, 
+        inputs[1][0].value, inputs[1][1].value, inputs[1][2].value,
+        inputs[2][0].value, inputs[2][1].value, inputs[2][2].value],
+        1: [inputs[0][3].value, inputs[0][4].value, inputs[0][5].value, 
+        inputs[1][3].value, inputs[1][4].value, inputs[1][5].value,
+        inputs[2][3].value, inputs[2][4].value, inputs[2][5].value],
+        2: [inputs[0][6].value, inputs[0][7].value, inputs[0][8].value, 
+        inputs[1][6].value, inputs[1][7].value, inputs[1][8].value,
+        inputs[2][6].value, inputs[2][7].value, inputs[2][8].value],
+        3: [inputs[3][0].value, inputs[3][1].value, inputs[3][2].value, 
+        inputs[4][0].value, inputs[4][1].value, inputs[4][2].value,
+        inputs[5][0].value, inputs[5][1].value, inputs[5][2].value],
+        4: [inputs[3][3].value, inputs[3][4].value, inputs[3][5].value, 
+        inputs[4][3].value, inputs[4][4].value, inputs[4][5].value,
+        inputs[5][3].value, inputs[5][4].value, inputs[5][5].value],
+        5: [inputs[3][6].value, inputs[3][7].value, inputs[3][8].value, 
+        inputs[4][6].value, inputs[4][7].value, inputs[4][8].value,
+        inputs[5][6].value, inputs[5][7].value, inputs[5][8].value],
+        6: [inputs[6][0].value, inputs[6][1].value, inputs[6][2].value, 
+        inputs[7][0].value, inputs[7][1].value, inputs[7][2].value,
+        inputs[8][0].value, inputs[8][1].value, inputs[8][2].value],
+        7: [inputs[6][3].value, inputs[6][4].value, inputs[6][5].value, 
+        inputs[7][3].value, inputs[7][4].value, inputs[7][5].value,
+        inputs[8][3].value, inputs[8][4].value, inputs[8][5].value],
+        8: [inputs[6][6].value, inputs[6][7].value, inputs[6][8].value, 
+        inputs[7][6].value, inputs[7][7].value, inputs[7][8].value,
+        inputs[8][6].value, inputs[8][7].value, inputs[8][8].value],
+    }
+
+    if (1 <= numero && numero <= 9) {
+
+        if (i === 0) {
+            if (0 <= k && k <= 2) {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[0], cuadrilateros[0].indexOf(cuadrilateros[0][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                k = k - 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[1], cuadrilateros[1].indexOf(cuadrilateros[1][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                k = k - 6
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[2], cuadrilateros[2].indexOf(cuadrilateros[2][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 1) {
+            if (0 <= k && k <= 2) {
+                k = k + 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[0], cuadrilateros[0].indexOf(cuadrilateros[0][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[1], cuadrilateros[1].indexOf(cuadrilateros[1][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                k = k - 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[2], cuadrilateros[2].indexOf(cuadrilateros[2][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 2) {
+            if (0 <= k && k <= 2) {
+                k = k + 6
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[0], cuadrilateros[0].indexOf(cuadrilateros[0][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                k = k + 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[1], cuadrilateros[1].indexOf(cuadrilateros[1][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[2], cuadrilateros[2].indexOf(cuadrilateros[2][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 3) {
+            if (0 <= k && k <= 2) {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[3], cuadrilateros[3].indexOf(cuadrilateros[3][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                k = k - 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[4], cuadrilateros[4].indexOf(cuadrilateros[4][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                k = k - 6
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[5], cuadrilateros[5].indexOf(cuadrilateros[5][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 4) {
+            if (0 <= k && k <= 2) {
+                k = k + 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[3], cuadrilateros[3].indexOf(cuadrilateros[3][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[4], cuadrilateros[4].indexOf(cuadrilateros[4][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                k = k - 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[5], cuadrilateros[5].indexOf(cuadrilateros[5][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 5) {
+            if (0 <= k && k <= 2) {
+                k = k + 6
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[3], cuadrilateros[3].indexOf(cuadrilateros[3][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                k = k + 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[4], cuadrilateros[4].indexOf(cuadrilateros[4][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[5], cuadrilateros[5].indexOf(cuadrilateros[5][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 6) {
+            if (0 <= k && k <= 2) {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[6], cuadrilateros[6].indexOf(cuadrilateros[6][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                k = k - 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[7], cuadrilateros[7].indexOf(cuadrilateros[7][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                k = k - 6
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[8], cuadrilateros[8].indexOf(cuadrilateros[8][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 7) {
+            if (0 <= k && k <= 2) {
+                k = k + 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[6], cuadrilateros[6].indexOf(cuadrilateros[6][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[7], cuadrilateros[7].indexOf(cuadrilateros[7][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                k = k - 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[8], cuadrilateros[8].indexOf(cuadrilateros[8][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        } else if (i === 8) {
+            if (0 <= k && k <= 2) {
+                k = k + 6
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[6], cuadrilateros[6].indexOf(cuadrilateros[6][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else if (3 <= k && k <= 5) {
+                k = k + 3
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[7], cuadrilateros[7].indexOf(cuadrilateros[7][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            } else {
+                var arrAux = arrayWithoutElementAtIndex(cuadrilateros[8], cuadrilateros[8].indexOf(cuadrilateros[8][k]))
+                if (arrAux.includes(numero)) {
+                    return true
+                }
+            }
+        }
+        // var arrAux = arrayWithoutElementAtIndex(cuadrilateros[x], cuadrilateros[x].indexOf(cuadrilateros[x][y]))
+        
+    }
+
+    return false
+}
+  
+function checkearTableroAll(inputOriginal, inputs) {
+    let numero = inputOriginal.value
+    if (!(1 <= numero && numero <= 9)) {
+        inputOriginal.value = ""
+    }
+    for (let fila of inputs) {
+        let indexY = 0
+        for (let inp of fila) {
+            checkearTablero(inp, inputs.indexOf(fila), indexY, inputs)
+            indexY += 1
+        }
+    }
+}
+
+function checkearTablero(inputOriginal, x, y, inputs) {
+    let numero = inputOriginal.value
+    // filas
+    let filas = arrayWithoutElementAtIndex(inputs[x], inputs[x].indexOf(inputOriginal))
+    for (let inp of filas) {
+        if (numero === inp.value) {
+            if (1 <= numero && numero <= 9) {
+                inputOriginal.style.color = "red"
+                return false
+            }
+        }
+        inputOriginal.style.color = "black"
+    }
+    // columnas
+    for (let j of Array.range(0, 8)) {
+        if (j === x) {
+            continue
+        } else {
+            if (numero === inputs[j][y].value) {
+                if (1 <= numero && numero <= 9) {
+                    inputOriginal.style.color = "red"
+                    return false
+                }
+            }
+        }
+        inputOriginal.style.color = "black"
+    }
+    // cuadrilatero
+    if (enCuadrilatero(numero, x, y, inputs)) {
+        if (1 <= numero && numero <= 9) {
+            inputOriginal.style.color = "red"
+            return false
+        }
+    } 
+    inputOriginal.style.color = "black"
+}
+
+function asignarColor(casilla, valor, fila, tablero_resuelto) {
+    if ((0 <= fila.indexOf(valor) <= 2) && (0 <= tablero_resuelto.indexOf(fila) <= 2)) {
+        casilla.className = "casilla par"
+    } else {
+        casilla.className = "casilla"
+    }
+}
+
+function generarTablero(htmlId, tablero_resuelto, dificultad) {
+    var inputs = []
+
+    for (let fila of tablero_resuelto) {
+        var fila_inputs = []
+        var row = document.createElement("div");
+        row.className = "row d-flex p-0 m-0";
+        row.id = `fila-${tablero_resuelto.indexOf(fila)}`
+        document.getElementById(htmlId).appendChild(row);
+        for (let valor of fila) {
+            var casilla = document.createElement("input")
+            casilla.type = "text"
+            casilla.value = valor
+            if (0 <= tablero_resuelto.indexOf(fila) && tablero_resuelto.indexOf(fila) <= 2) {
+                if (0 <= fila.indexOf(valor) && fila.indexOf(valor) <= 2) {
+                    casilla.className = "casilla par"
+                } else if (3 <= fila.indexOf(valor) && fila.indexOf(valor) <= 5) {
+                    casilla.className = "casilla"
+                } else {
+                    casilla.className = "casilla par"
+                }
+            } else if (3 <= tablero_resuelto.indexOf(fila) && tablero_resuelto.indexOf(fila) <= 5) {
+                if (0 <= fila.indexOf(valor) && fila.indexOf(valor) <= 2) {
+                    casilla.className = "casilla"
+                } else if (3 <= fila.indexOf(valor) && fila.indexOf(valor) <= 5) {
+                    casilla.className = "casilla par"
+                } else {
+                    casilla.className = "casilla"
+                }
+            } else {
+                if (0 <= fila.indexOf(valor) && fila.indexOf(valor) <= 2) {
+                    casilla.className = "casilla par"
+                } else if (3 <= fila.indexOf(valor) && fila.indexOf(valor) <= 5) {
+                    casilla.className = "casilla"
+                } else {
+                    casilla.className = "casilla par"
+                }
+            }
+            casilla.disabled = true
+            document.getElementById(`fila-${tablero_resuelto.indexOf(fila)}`).appendChild(casilla)
+            fila_inputs.push(casilla)
+        }
+        inputs.push(fila_inputs)
+    }
+
+    var contador = 0
+    var casillas_utilizadas = []
+
+    while (contador <= dificultad) {
+        var x = Array.range(0, 8)[Math.floor(Math.random()*Array.range(0, 8).length)]
+        var y = Array.range(0, 8)[Math.floor(Math.random()*Array.range(0, 8).length)]
+        if (casillas_utilizadas.includes(inputs[x][y])) {
+            continue
+        }
+        inputs[x][y].disabled = false
+        inputs[x][y].value = ""
+        casillas_utilizadas.push(inputs[x][y])
+        contador += 1
+    }
+
+    for (let fila of inputs) {
+        for (let inp of fila) {
+            inp.addEventListener('keyup', function () {
+                checkearTableroAll(inp, inputs)
+            }) 
+        }
+    }
+
+    document.getElementById("timer").style.color = "white"
+
+}
+
+window.onLoad = generarTablero("tablero", nuevoTablero(), 60)
+
+async function funcionTimer(fechaPrevia) {
+    setTimeout(() => {
+        funcionTimer(fechaPrevia);
+      }, 100);
+    let tiempoSegundos = (new Date().getTime()/1000 - fechaPrevia).toFixed(1)
+    let tiempoMinutos = parseInt(tiempoSegundos / 60)
+    let tiempoHoras = parseInt(tiempoMinutos / 60)
+    document.getElementById("timer").innerHTML = `Tiempo: ${(tiempoHoras < 10) 
+        ? '0' + (tiempoHoras) : tiempoHoras}:${((tiempoMinutos - 
+            parseInt(60*tiempoHoras)) < 10) ? ("0" + (tiempoMinutos - 
+                parseInt(60*tiempoHoras))) : (tiempoMinutos 
+                    - parseInt(60*tiempoHoras))}:${((tiempoSegundos 
+                        - parseInt(60*tiempoMinutos)).toFixed(1) < 10) 
+                        ? ("0" + (tiempoSegundos - parseInt(60*tiempoMinutos)
+                        ).toFixed(1)) : (tiempoSegundos - parseInt(60*tiempoMinutos)
+                        ).toFixed(1)}`
+}
+
+let flag = false
+
+document.onkeypress = inicioTimer
+
+function inicioTimer() {
+    if (!flag) {
+        document.getElementById("timer").style.setProperty("opacity", "1", "important");
+        funcionTimer(new Date().getTime()/1000)
+        flag = true
+    }
+}

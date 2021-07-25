a7393bebb247836e22d2bd69068449d754cbcd22
augusto-rehfeldt <augusto.rehfeldt.socio@gmail.com>
2021-07-25 15:02:11 -0300

    1.0.2

--- index.js
+++ index.js
@@ -620,7 +620,7 @@ function generarTablero(htmlId, tablero_resuelto, dificultad) {
 
 }
 
-window.onLoad = generarTablero("tablero", nuevoTablero(), 60)
+window.onLoad = generarTablero("tablero", nuevoTablero(), 55)
 
 
 async function funcionTimer(fechaPrevia) {
